48f5a74ad395e08d71ea31eaa50ffeb8a31970f1
T3r1jj <t3r1jj@gmail.com>
2018-02-23 14:21:22 +0100

    Initial commit

---
+++ app/src/main/java/io/github/t3r1jj/ips/collector/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.t3r1jj.ips.collector
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
